7d7feb143e6c9b00a7a8babfc06e581a07972a01
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-19 20:27:11 +0700

    feat: Inject ViewModel into Home screen and pass state

--- app/src/main/java/dev/alejo/colombian_holidays/ui/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/core/navigation/NavigationWrapper.kt
@@ -1,16 +1,22 @@
 package dev.alejo.colombian_holidays.ui.core.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import dev.alejo.colombian_holidays.ui.home.HomeScreen
+import dev.alejo.colombian_holidays.ui.home.HomeViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun NavigationWrapper(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Routes.Home.route) {
         composable(Routes.Home.route) {
-            HomeScreen()
+            val viewModel = koinViewModel<HomeViewModel>()
+            val state by viewModel.state.collectAsState()
+            HomeScreen(state)
         }
     }
 }
