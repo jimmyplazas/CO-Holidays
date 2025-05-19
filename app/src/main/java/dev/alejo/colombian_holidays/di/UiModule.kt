80bcf97f649459a2df8c6a1bd885e8d724c463cb
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-19 20:27:16 +0700

    feat: Add HomeViewModel to Koin module

--- app/src/main/java/dev/alejo/colombian_holidays/di/UiModule.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/di/UiModule.kt
@@ -1,7 +1,9 @@
 package dev.alejo.colombian_holidays.di
 
+import dev.alejo.colombian_holidays.ui.home.HomeViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val uiModule = module {
-
+    viewModelOf(::HomeViewModel)
 }
