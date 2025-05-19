187323aae7c0d2aaf8f9bc5deecb94b2ded3afc7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-19 20:26:59 +0700

    feat: Add background image to HomeScreen

--- app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeScreen.kt
@@ -1,13 +1,20 @@
 package dev.alejo.colombian_holidays.ui.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 @Composable
-fun HomeScreen() {
-    Box(Modifier.fillMaxSize().background(Color.Cyan))
+fun HomeScreen(state: HomeState) {
+    Box(Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = state.backgroundImage),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+    }
 }
