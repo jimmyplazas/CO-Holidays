0ac2ab5628db0dbd7fc45bc8f5345b44df9417f2
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-19 20:26:06 +0700

    feat: Create HomeState with default background image

---
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeState.kt
@@ -0,0 +1,8 @@
+package dev.alejo.colombian_holidays.ui.home
+
+import androidx.annotation.DrawableRes
+import dev.alejo.colombian_holidays.R
+
+data class HomeState(
+    @DrawableRes val backgroundImage: Int = R.drawable.img_0
+)
