a588900a20aa4c739af59c84b7c3c74639e843b0
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-20 17:47:03 +0700

    feat: Add ImagesProvider and inject it

--- app/src/main/java/dev/alejo/colombian_holidays/di/UiModule.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/di/UiModule.kt
@@ -1,9 +1,11 @@
 package dev.alejo.colombian_holidays.di
 
 import dev.alejo.colombian_holidays.ui.home.HomeViewModel
+import dev.alejo.colombian_holidays.ui.util.ImagesProvider
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val uiModule = module {
+    single { ImagesProvider }
     viewModelOf(::HomeViewModel)
 }
