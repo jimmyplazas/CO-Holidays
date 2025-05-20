e02984f6fd9ac31589b0f76bd24238b0659916a7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-20 17:45:56 +0700

    feat: Update HomeState with holiday data and loading states

--- app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeState.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeState.kt
@@ -2,7 +2,18 @@ package dev.alejo.colombian_holidays.ui.home
 
 import androidx.annotation.DrawableRes
 import dev.alejo.colombian_holidays.R
+import dev.alejo.colombian_holidays.domain.model.PublicHolidayModel
+import java.time.LocalDate
 
 data class HomeState(
-    @DrawableRes val backgroundImage: Int = R.drawable.img_0
+    @DrawableRes val backgroundImage: Int = R.drawable.img_0,
+    val holidays: List<PublicHolidayModel> = emptyList(),
+    val nextHoliday: PublicHolidayModel? = null,
+    val isLoadingNextHoliday: Boolean = true,
+    val isLoadingHolidays: Boolean = true,
+    val isLoadingTodayHoliday: Boolean = true,
+    val todayHoliday: PublicHolidayModel? = null,
+    val lastTodayHolidayResponse: LocalDate? = null,
+    val isDataLoaded: Boolean = false,
+    val error: String? = null
 )
