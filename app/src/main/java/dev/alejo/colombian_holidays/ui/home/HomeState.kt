aa0ee5a6784ac9fe9ab3f1550daf7c2261f2620f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-20 23:06:32 +0700

    feat: Integrate Material 3 calendar and improve home screen«

--- app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeState.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/home/HomeState.kt
@@ -8,6 +8,7 @@ import java.time.LocalDate
 data class HomeState(
     @DrawableRes val backgroundImage: Int = R.drawable.img_0,
     val holidays: List<PublicHolidayModel> = emptyList(),
+    val loadedYears: Set<Int> = emptySet(),
     val nextHoliday: PublicHolidayModel? = null,
     val isLoadingNextHoliday: Boolean = true,
     val isLoadingHolidays: Boolean = true,
@@ -15,5 +16,6 @@ data class HomeState(
     val todayHoliday: PublicHolidayModel? = null,
     val lastTodayHolidayResponse: LocalDate? = null,
     val isDataLoaded: Boolean = false,
+    val currentMonth: LocalDate = LocalDate.now(),
     val error: String? = null
 )
