338675f50c4d8f3d58d99e5493194c4d0fb94fe9
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-20 17:44:11 +0700

    feat: Create AppDimens object for standard dimensions

---
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/theme/AppDImens.kt
@@ -0,0 +1,12 @@
+package dev.alejo.colombian_holidays.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppDimens {
+    val Small = 8.dp
+    val Default = 16.dp
+    val Large = 24.dp
+    val ExtraLarge = 32.dp
+    val DefaultPeekHeight = 90.dp
+    val PeakSheetTopMargin = 120.dp
+}
