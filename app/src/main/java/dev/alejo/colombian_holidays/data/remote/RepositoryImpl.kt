ce70f4c3b4fee4e2bbdd5737c4eaa110083f29e4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-23 17:52:58 +0700

    refactor: Remove unused methods from RepositoryImpl

--- app/src/main/java/dev/alejo/colombian_holidays/data/remote/RepositoryImpl.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/data/remote/RepositoryImpl.kt
@@ -4,31 +4,10 @@ import dev.alejo.colombian_holidays.core.Response
 import dev.alejo.colombian_holidays.data.remote.response.toDomain
 import dev.alejo.colombian_holidays.domain.model.PublicHolidayModel
 import dev.alejo.colombian_holidays.domain.repository.Repository
-import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class RepositoryImpl(private val api: ApiService) : Repository {
-    override suspend fun isTodayHoliday(): Response<Boolean> = withContext(Dispatchers.IO) {
-        try {
-            val response = api.isTodayHoliday()
-            when (response.status) {
-                HttpStatusCode.OK -> {
-                    Response.Success(data = true)
-                }
-
-                HttpStatusCode.NoContent -> {
-                    Response.Success(data = false)
-                }
-
-                else -> {
-                    Response.Error("Error inesperado")
-                }
-            }
-        } catch (e: Exception) {
-            Response.Error(e.message ?: "Ha ocurrido un error")
-        }
-    }
 
     override suspend fun getHolidaysByYear(
         year: String
@@ -41,13 +20,4 @@ class RepositoryImpl(private val api: ApiService) : Repository {
         }
     }
 
-    override suspend fun getNextPublicHoliday(): Response<PublicHolidayModel> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = api.getNextPublicHoliday()
-                Response.Success(data = response.first().toDomain())
-            } catch (e: Exception) {
-                Response.Error(e.message ?: "Ha ocurrido un error")
-            }
-        }
 }
