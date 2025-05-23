f816aea730e389561755e5a0c2b7839bfe004f7b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-23 17:52:30 +0700

    Update date property in PublicHolidayModel to LocalDate

--- app/src/main/java/dev/alejo/colombian_holidays/domain/model/PublicHolidayModel.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/domain/model/PublicHolidayModel.kt
@@ -1,7 +1,9 @@
 package dev.alejo.colombian_holidays.domain.model
 
+import java.time.LocalDate
+
 data class PublicHolidayModel(
-    val date: String,
+    val date: LocalDate,
     val localName: String,
     val name: String,
     val global: Boolean,
