5b48b773e8ff1acda29d8771d2bb842d51dadea9
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-24 17:35:33 +0700

    feat: Add more AppDimens

--- app/src/main/java/dev/alejo/colombian_holidays/ui/theme/AppDImens.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/theme/AppDImens.kt
@@ -3,10 +3,16 @@ package dev.alejo.colombian_holidays.ui.theme
 import androidx.compose.ui.unit.dp
 
 object AppDimens {
+    val None = 0.dp
+    val ExtraSmall = 4.dp
     val Small = 8.dp
     val Default = 16.dp
     val Large = 24.dp
     val ExtraLarge = 32.dp
+    val NoHolidaysHorizontalPadding = 42.dp
+    val ListBottomNavigationHeight = 56.dp
+    val ListBottomPadding = 60.dp
+    val ListBottomNavigationPadding = 32.dp
     val DefaultPeekHeight = 120.dp
     val PeakSheetTopMargin = 90.dp
 }
