fba0703dee5cfdcf81d7d713ad63ded1ed1d32ea
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-24 17:35:50 +0700

    feat: Implement CalendarScreen with holiday display

--- app/src/main/java/dev/alejo/colombian_holidays/ui/home/components/CalendarScreen.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/ui/home/components/CalendarScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,9 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import dev.alejo.colombian_holidays.R
 import dev.alejo.colombian_holidays.domain.model.PublicHolidayModel
+import dev.alejo.colombian_holidays.ui.home.HolidaysAction
 import dev.alejo.colombian_holidays.ui.theme.AppDimens
 import dev.alejo.compose_calendar.CalendarEvent
 import dev.alejo.compose_calendar.ComposeCalendar
@@ -29,51 +34,60 @@ import java.time.LocalDate
 fun CalendarScreen(
     holidays: List<CalendarEvent<PublicHolidayModel>>,
     currentMonth: LocalDate,
-    onPreviousMonth: () -> Unit,
-    onNextMonth: () -> Unit
+    onHolidaysAction: (HolidaysAction) -> Unit
 ) {
-    ComposeCalendar(
-        events = holidays,
-        calendarColors = CalendarDefaults.calendarColors(
-            eventBackgroundColor = MaterialTheme.colorScheme.primaryContainer,
-            eventContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-        ),
-        onDayClick = { _, _ -> },
-        onPreviousMonthClick = { onPreviousMonth() },
-        onNextMonthClick = { onNextMonth() },
-        eventIndicator = {_, _, _ ->
-            HolidayIndicator()
-        },
-        maxIndicators = CalendarDefaults.IndicatorLimit.Two
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = AppDimens.Default),
+        verticalArrangement = Arrangement.spacedBy(AppDimens.Default),
+    ) {
+        val monthHolidays = holidays
+            .filter { it.date.month == currentMonth.month && it.date.year == currentMonth.year }
 
-    val monthHolidays = holidays
-        .filter { it.data!!.date.month == currentMonth.month && it.data!!.date.year == currentMonth.year }
+        ComposeCalendar(
+            events = holidays,
+            initDate = currentMonth,
+            calendarColors = CalendarDefaults.calendarColors(
+                eventBackgroundColor = MaterialTheme.colorScheme.primaryContainer,
+                eventContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ),
+            onDayClick = { _, _ -> },
+            onPreviousMonthClick = { onHolidaysAction(HolidaysAction.PreviousMonth) },
+            onNextMonthClick = { onHolidaysAction(HolidaysAction.NextMonth) },
+            eventIndicator = { _, _, _ ->
+                HolidayIndicator()
+            },
+            maxIndicators = CalendarDefaults.IndicatorLimit.Two
+        )
 
-    AnimatedContent(monthHolidays.isEmpty()) { noHolidays ->
-        if (noHolidays) {
-            Image(
-                painter = painterResource(id = R.drawable.no_holidays_img),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = AppDimens.ExtraLarge),
-                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
-            )
-        } else {
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(AppDimens.Small),
-                contentPadding = PaddingValues(bottom = AppDimens.Default)
-            ) {
-                items(monthHolidays.size) { index ->
-                    HolidayItem(monthHolidays[index].data!!) {
+        AnimatedContent(targetState = monthHolidays.isEmpty()) { noHolidays ->
+            if (noHolidays) {
+                Box(Modifier.fillMaxWidth().padding(horizontal = AppDimens.NoHolidaysHorizontalPadding)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.no_holidays_img),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxWidth(),
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            } else {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(AppDimens.Small),
+                    contentPadding = PaddingValues(bottom = AppDimens.Default)
+                ) {
+                    items(monthHolidays.size) { index ->
+                        HolidayItem(monthHolidays[index].data!!) {
 
+                        }
                     }
                 }
             }
         }
     }
+
 }
 
 @Composable
@@ -83,5 +97,15 @@ fun HolidayIndicator() {
             .size(AppDimens.Small)
             .clip(CircleShape)
             .background(MaterialTheme.colorScheme.primary)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CalendarScreenPrev() {
+    CalendarScreen(
+        holidays = emptyList(),
+        currentMonth = LocalDate.now(),
+        onHolidaysAction = {}
     )
 }
