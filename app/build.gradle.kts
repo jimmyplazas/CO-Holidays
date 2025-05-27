fe1322b886e811964b9c64dc908591944d5e0fc3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-27 13:23:34 +0700

    Update version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "dev.alejo.colombian_holidays"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
