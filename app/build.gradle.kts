645ed44211283199ed1db123f978289b176f4f0b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-27 11:13:56 +0700

    Bump app version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.alejo.colombian_holidays"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
