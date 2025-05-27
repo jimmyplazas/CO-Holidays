1da486a1185df55ed617937200464e80af0c61ce
Jimmyale3102 <jimmyale3102@gmail.com>
2025-05-27 12:07:15 +0700

    chore: Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 3
-        versionName = "1.2.1"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
