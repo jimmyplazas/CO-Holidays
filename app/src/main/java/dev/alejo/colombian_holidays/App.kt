c5968743b279722b29889d606468f3719aeae97e
Jimmyale3102 <jimmyale3102@gmail.com>
2025-07-30 20:04:13 -0500

    feat: Schedule daily widget update

--- app/src/main/java/dev/alejo/colombian_holidays/App.kt
+++ app/src/main/java/dev/alejo/colombian_holidays/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dev.alejo.colombian_holidays.di.dataModule
 import dev.alejo.colombian_holidays.di.domainModule
 import dev.alejo.colombian_holidays.di.uiModule
+import dev.alejo.colombian_holidays.widget.WidgetUpdateScheduler
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -11,6 +12,7 @@ import org.koin.core.context.startKoin
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        WidgetUpdateScheduler.scheduleDailyUpdate(this)
         startKoin {
             androidLogger()
             androidContext(this@App)
